2ab9a9e50d96719250d651011706c725006428b5
Chiwai <chiwai.kiriba@gmail.com>
2025-05-30 09:23:45 +0300

    full star rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useState } from "react";
 import Star from "./Star";
 
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-red-50">
       <div className="bg-[#f5ffbe] rounded-xl shadow-xl text-center p-6 sm:p-10 md:p-14 lg:p-18">
@@ -9,14 +12,17 @@ const Rating = () => {
           Rate your Experience
         </h2>
         <div className="flex justify-center mb-4 gap-1 sm:gap-2">
-          <Star />
-          <Star />
-          <Star />
-          <Star />
-          <Star />
+          {Array.from({ length: 5 }, (_, index) => (
+            <Star
+              key={index}
+              index={index}
+              rating={rating}
+              setRating={setRating}
+            />
+          ))}
         </div>
         <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium text-gray-700 mb-6">
-          How was your experience?
+          {rating} out of 5
         </p>
       </div>
     </div>
--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const Star = () => {
+const Star = ({ index, rating, setRating }) => {
+  const handleClick = () => {
+    setRating(index + 1);
+  };
+
   return (
     <div>
-      <i className="bx bxs-star text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-green-400"></i>
+      <i
+        className={` bx ${
+          rating >= index + 1 ? "bxs-star" : "bx-star"
+        } text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-green-400`}
+        onClick={handleClick}
+      ></i>
     </div>
   );
 };
